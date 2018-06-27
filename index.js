28bf05aa4dd90d60824f42a887423ec3b91b955d
Roberto <rob.vicalvaro@gmail.com>
2018-06-28 00:26:50 +0200

    Subida final, funcionando, con imágenes

--- index.js
+++ index.js
@@ -9,29 +9,53 @@ function releaseContent(req, res) {
     res.setHeader("Content-type", "text/html");
     
     //Obtengo los valores que van a estar fijos
-    let validUrls = ["/", "/home", "/bio", "/servicios", "/contacto"];
-    let resourceUrls = ["/img", "/css", "/js"];
+    let validUrls = ["/index", "/bio", "/servicios", "/contacto"];
+    let allowHtml;
     let file;
+    let body;
     
     //Preparo el contenidop
     const header = fs.readFileSync("./templates/header.html");
-
-    if (validUrls.indexOf(req.url)) {
+ 
+    if (req.url === "/" || req.url === "") {
+        res.statusCode = 200;
+        file = 'index.html';
+        allowHtml = true;
+    }
+    else if (req.url.includes('css')) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        file = fs.readFileSync('.'+req.url, {encoding: 'utf8'});
+        res.write(file);
+        allowHtml = false;
+    }
+    else if (req.url.includes('img')) { 
+        res.statusCode = 200;
+        file = fs.readFileSync('.'+req.url);
+        res.write(file);
+        allowHtml = false; 
+    }
+    else if (req.url === "/img/favicon.ico" ) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "image/ico");
+        file = fs.readFileSync('./img/favicon.ico');
+        res.write(file);
+        allowHtml = false; 
+    }
+    else if (validUrls.indexOf(req.url) !== -1) {
         res.statusCode = 200;
         file = req.url+'.html';
-        if(req.url === "") { // No funciona esto si no tiene barra ni url???
-            file = 'index.html';
-        }
-    } else if (resourceUrls.indexOf(req.url) !== -1) {
-        res.statusCode = 200;
-        file = req.url;
-    }
+        allowHtml = true;
+    } 
     else {
         res.statusCode = 404; 
         file = '404.html'; 
+        allowHtml = true;
     }
-
-    let body = fs.readFileSync(`./templates/bodies/${file}`);
+    
+    if(allowHtml) {
+        body = fs.readFileSync(`./templates/bodies/${file}`);
+    }
     const footer = fs.readFileSync("./templates/footer.html");
      
     //Establezco el footer y lanzo
